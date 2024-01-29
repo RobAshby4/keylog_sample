de848712633ab09b8e1f81dd06f988625245ef3b
RobAshby4 <robert.n.ashby@gmail.com>
2024-01-29 15:43:59 -0800

    now only outputs on keydown

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdbool.h>
+#include <linux/input-event-codes.h>
 
 #define INTERFACE "/dev/input/event0"
 
@@ -14,12 +15,12 @@ typedef struct input_event {
 int main(int argc, char** argv) {
     FILE *fp = fopen(INTERFACE, "r");
     input_event keystroke;
-    while (1) {
-        size_t bytes_read;
-        bytes_read = fread(&keystroke, sizeof(input_event), 1, fp);
-        if (bytes_read > 0) {
-            printf("type:%d code:%d value:%d\n", keystroke.type, keystroke.code, keystroke.value);
-        }
+
+    size_t bytes_read;
+    while ((bytes_read = fread(&keystroke, sizeof(input_event), 1, fp)) != 0) {
+        if (keystroke.type == 1 && keystroke.value == 1)
+        printf("type:%d code:%d value:%d\n", keystroke.type, keystroke.code, keystroke.value);
+        // TODO: Translate keycodes to string
     }
 }
 
